83bb455adba7a3b9d64c0d66828c1d2e4fbbb6bd
Vladimir <UglyBob23@yandex.ru>
2020-08-04 16:01:02 +0500

    Initial Commit

---
+++ ContactListV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContactListV2
+//
+//  Created by Владимир Паутов on 04.08.2020.
+//  Copyright © 2020 Владимир Паутов. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
